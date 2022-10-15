13a3fdcf4e4b4a31d012e3b3d0a7777fc5be7fec
Tasie Ibiam <tasieibiam28@gmail.com>
2022-10-15 11:55:24 -0700

    Random numbers of different signs

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,20 +1,28 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-main(void)
+/**
+ * main - prints +ve, -ve numbers or zero randomly
+ * Return: 0
+ */
+int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
-	} else if (n == 0)
+	}
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
-	} else
-	{	printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
