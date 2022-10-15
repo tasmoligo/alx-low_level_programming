447def5e513299f52de20ad92440dcab0f979cf2
Tasie Ibiam <tasieibiam28@gmail.com>
2022-10-15 13:55:46 -0700

    Single digits of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints from 0 to 9
+ * Return:0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
