d338e0560d8bd9f074116cba0bcd7ba162e2901c
Tasie Ibiam <tasieibiam28@gmail.com>
2022-10-15 13:24:00 -0700

    Lower and upper case alphabeths

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - lower and uppercase alphas
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
