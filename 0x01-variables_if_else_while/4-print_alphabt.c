f2187f3e1e3f365b2578af991c9c686a2e155687
Tasie Ibiam <tasieibiam28@gmail.com>
2022-10-15 13:36:21 -0700

    Alphabeths without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints lowercase letters except q and e
+ * Return:0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
