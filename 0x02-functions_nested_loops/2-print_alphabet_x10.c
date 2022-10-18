f0a93468efc30434f58eddba78f4b06bc2a6105a
Tasie Ibiam <tasieibiam28@gmail.com>
2022-10-18 14:09:25 -0700

    10x lower case alphabeths

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10x lower cased alphabeths
+ */
+void print_alphabet_x10(void)
+{
+	int n;
+	char c;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+}
