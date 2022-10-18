bba2446ea5366b6e60dfcd0b99babe293f08f238
Tasie Ibiam <tasieibiam28@gmail.com>
2022-10-18 13:32:42 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - displays character to standard output
+ * Return: 1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,6 +14,8 @@ int add(int, int);
 void print_to_98(int n);
 int _putchar(char c);
 
+#endif
+
 	
 	
 		
