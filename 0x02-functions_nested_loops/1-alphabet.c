8274524ed13cee1d09fcf4c68488a3a74bafa77d
Tasie Ibiam <tasieibiam28@gmail.com>
2022-10-18 13:43:55 -0700

    Lowercased alphabeths

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - prints lowercased alphabeths
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+	_putchar('\n');
+}
