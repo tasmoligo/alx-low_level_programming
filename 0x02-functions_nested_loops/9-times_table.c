e35d24bc1b7cf844615b56dcda025ca802e597c6
Tasie Ibiam <tasieibiam28@gmail.com>
2022-10-19 13:23:08 -0700

    Prints the 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * times_table - Prints 9 times table
+ */
+void times_table(void)
+{
+	int n, mult, prod;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar('0');
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			prod = n * mult;
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
+
