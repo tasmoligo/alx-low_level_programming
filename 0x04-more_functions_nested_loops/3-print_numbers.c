4956185ca817075611280ab935b4203c9bfeda3d
Tasie Ibiam <tasieibiam28@gmail.com>
2022-10-22 11:31:38 -0700

    git cpmmit -m

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - chechs the code
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		_putchar(a++ + '0');
+	}
+	_putchar('\n');
+}
