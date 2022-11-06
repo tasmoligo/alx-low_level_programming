add2a4ff38fd50af618bddfb45e701e9bac85a00
Tasie Ibiam <tasieibiam28@gmail.com>
2022-11-06 12:39:13 -0800

    Print the name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,12 +3,12 @@
 
 /**
  * main - prints program name
- * @c: number of arguments
- * @v: the argument vector
+ * @argc: number of arguments
+ * @argv: the argument vector
  *
  * Return: 0 on success
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
