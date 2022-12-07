9217d42865423d2238ab9048e46203894d6dfb4c
Tasie Ibiam <tasieibiam28@gmail.com>
2022-12-06 18:39:23 -0800

    Tasks 1 and 2 modified

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,18 +11,22 @@ int create_file(const char *filename, char *text_content)
 {
 	int o, w, i = 0;
 
+	o = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (o == -1)
+	{
+		return (-1);
+	}
 	if (filename == NULL)
 	{
 		return (-1);
 	}
 	if (text_content != NULL)
+	for (i = 0; text_content[i]; i++)
 	{
-		for (i = 0; text_content[i];)
-			i++;
+		;
 	}
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(STDIN_FILENO, text_content, i);
-	if (o == -1 || w == -1)
+	w = write(o, text_content, i);
+	if (w == -1)
 	{
 		return (-1);
 	}
