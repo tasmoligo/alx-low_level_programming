5c06a30510a5cce991947a59d1a5299086ee2daf
Tasie Ibiam <tasieibiam28@gmail.com>
2022-12-06 17:30:01 -0800

    Task 1: 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
   */
 int create_file(const char *filename, char *text_content)
 {
-	int len, o, w;
+	int o, w, i = 0;
 
 	if (filename == NULL)
 	{
@@ -17,11 +17,11 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (i = 0; text_content[i];)
+			i++;
 	}
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(STDIN_FILENO, text_content, len);
+	w = write(STDIN_FILENO, text_content, i);
 	if (o == -1 || w == -1)
 	{
 		return (-1);
