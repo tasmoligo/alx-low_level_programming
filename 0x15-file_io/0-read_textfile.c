9fbeea87621af11b9d4f53c3082d9b99cdf4d9da
Tasie Ibiam <tasieibiam28@gmail.com>
2022-12-06 18:57:07 -0800

    Task 0 modified

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,21 +15,30 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-	buff = malloc(sizeof(char) * letters);
-	if (buff == NULL)
+	o = open(filename, O_RDONLY);
+	if (o == -1)
 	{
 		return (0);
 	}
-	r = read(STDIN_FILENO, buff, letters);
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+	{
+		close(o);
+		return (0);
+	}
+	r = read(o, buff, letters);
+	if (r == -1)
+	{
+		free(buff);
+		return (0);
+	}
 	w = write(STDOUT_FILENO, buff, r);
-	o = open(filename, O_RDONLY);
-	if (r == -1 || w == -1 || o == -1 || w != r)
+	if (w != r)
 	{
 		free(buff);
 		return (0);
 	}
 	free(buff);
-	close(o);
 	return (w);
 }
 
