844d88576265dec63a934b9512c06dd4e5f7369f
Tasie Ibiam <tasieibiam28@gmail.com>
2022-12-31 06:37:36 -0800

    Main file and header file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
+ * main - tests function that prints if integer is positive, negative or zero
  * Return: 0
  */
 
 int main(void)
 {
-	int i;
+	int n;
 
-	i = 0;
-	positive_or_negative(i);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	positive_or_negative(n);
 
 	return (0);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
+
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
