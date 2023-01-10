e91b0cab4e008d950279779cfdca21b1ad5459fb
Tasie Ibiam <tasieibiam28@gmail.com>
2023-01-10 00:52:50 -0800

    Task 7 corrected

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,7 +10,9 @@ int print_last_digit(int n)
 	int l = n % 10;
 
 	if (l < 0)
-		l += -1;
-	_putchar(l + '0');
-	return (0);
+    {
+        l *= -1;
+    }
+    _putchar(l + '0');
+    return (l);
 }
