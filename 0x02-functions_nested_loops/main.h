898a958086ba165d21e06a4256168b0cf33f8e4a
Tasie Ibiam <tasieibiam28@gmail.com>
2023-01-10 00:29:00 -0800

    Main header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -13,10 +17,6 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 int _putchar(char c);
-void print_times_table(int n)
+void print_times_table(int n);
 
 #endif
-
-	
-	
-		
