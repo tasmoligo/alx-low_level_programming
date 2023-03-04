d05abe1729529267ac24cf7663d5aeaccdaf6ab6
Tasie Ibiam <113722054+tasmoligo@users.noreply.github.com>
2023-03-04 22:38:44 +0100

    The header

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
