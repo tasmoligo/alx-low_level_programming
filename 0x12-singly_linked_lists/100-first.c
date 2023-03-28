b624c3b82a8a5116e8189b751670bec7635aab63
Tasie Ibiam <tasieibiam28@gmail.com>
2023-03-28 08:29:03 -0700

    printing before main execution

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@
   */
 void __attribute__ ((constructor)) before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon back!\n");
+	printf("You're beat! and yet, you must allow,\nI");
+	printf(" bore my house upon my back!\n");
 }
