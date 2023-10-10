2df3e6eea259c93b20779b46dc27a9406c870cbd
Tasie Ibiam <tasieibiam28@gmail.com>
2023-10-10 13:08:56 -0700

    defining a struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define _DOG_H_
 
 /**
-  * 
+  * struct dog - the dog structure
   * @nane : the pointer to locate the dogs n' ame
   * @owner : the pointer to locate the dogs ' owner
   * @age : member to hold the dogs a' ge
