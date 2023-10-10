6440424223ae13ff037a36a9a4fd6c4dbfbcf329
Tasie Ibiam <tasieibiam28@gmail.com>
2023-10-10 13:01:38 -0700

    defining a struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,6 @@
+<h1>Structures, typedef</h1>
+<p>Struct and typedef are used to create a new user defined data type. Their definitions are as follows:</p>
+<p>struct name<br>
+{<br>	type name1;<br>
+	type name2;<br>
+	&#47*You can declare as many members as deemed fit*&#47<br>};</p>
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+  * 
+  * @nane : the pointer to locate the dogs n' ame
+  * @owner : the pointer to locate the dogs ' owner
+  * @age : member to hold the dogs a' ge
+  */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
