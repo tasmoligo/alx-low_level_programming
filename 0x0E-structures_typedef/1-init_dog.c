8726b733cbe219b5904f290ec32eb97eb45bda14
Tasie Ibiam <tasieibiam28@gmail.com>
2023-10-10 15:27:16 -0700

    Hmm! Accessing the members of a struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+  * init_dog - initializes a dog struct
+  * @d: the pointer to the struct members
+  * @name: the name of the dog
+  * @age:the age of the dog
+  * @owner: the owner of the dog
+  */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).owner = owner;
+	(*d).age = age;
+}
