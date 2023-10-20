ce16ec30fc7f6c99d9f9016b1628139dc3172087
Tasie Ibiam <tasieibiam28@gmail.com>
2023-10-19 23:49:20 -0700

    executing before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+  * b_main - executes before main function
+  * Return: nothing
+  */
+void b_main(void)
+{
+	printf("You're beat! and yet, you must allow,
+	\nI bore my house upon my back!\n");
+}
