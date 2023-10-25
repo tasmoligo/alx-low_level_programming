bb63056e1ed253a3271fb1915911905580bb1be0
Tasie Ibiam <tasieibiam28@gmail.com>
2023-10-24 22:11:01 -0700

    inserting node at beginning

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,10 +10,6 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 
-	if (str == NULL)
-	{
-		return (NULL);
-	}
 	if (head == NULL)
 	{
 		return (NULL);
@@ -24,11 +20,6 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
 	new_node->len = _strlen(str);
 	new_node->next = *head;
 	*head = new_node;
