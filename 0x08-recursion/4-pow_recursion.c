0cd7e20e7494904eff25bda7935c0e48565bf659
Tasie Ibiam <tasieibiam28@gmail.com>
2023-10-26 01:03:12 -0700

    finding the value of a number raised to another number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _pow_recursion - returns x raised to y
+  * @x: the base number
+  * @y: the power
+  * Return: value of x raised to y
+  */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x , y - 1));
+}
