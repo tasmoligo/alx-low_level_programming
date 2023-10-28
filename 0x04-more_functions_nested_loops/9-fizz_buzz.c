6cfea10fdaadf67fb2733ddb74da0fc189bfc7a0
Tasie Ibiam <tasieibiam28@gmail.com>
2023-10-27 18:26:09 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,28 +11,21 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-		if (n < 100)
+		if (n % 3 == 0 && n % 5 == 0)
 		{
-			if (n % 3 == 0 && n % 5 == 0)
-			{
-				printf("fizzBuzz ");
-			}
-			else if (n % 3 == 0)
-			{
-				printf("Fizz ");
-			}
-			else if (n % 5 == 0)
-			{
-				printf("Buzz ");
-			}
-			else
-			{
-				printf("%d ", n);
-			}
+			printf("fizzBuzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
 		}
 		else
 		{
-			printf("Buzz\n");
+			printf("%d ", n);
 		}
 	}
 	printf("\n");
