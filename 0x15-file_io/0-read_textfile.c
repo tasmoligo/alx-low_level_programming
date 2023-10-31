f36e503de34ce62b7657c1890228e0b33696fbb2
Tasie Ibiam <tasieibiam28@gmail.com>
2023-10-30 20:36:32 -0700

    reading from a file and printing to standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+  * read_textfile - reads a text file and prints
+  *		it to the POSIX standard output.
+  * @filename: file it should read from
+  * @letters: number of letters it should read and print
+  * Return: actual number of letters it could read and print,
+  *	0 on failure
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t bytes_read, bytes_written;
+	char *buff;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	buff = malloc(letters);
+	if (buff == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	bytes_read = read(fd, buff, letters);
+	if (bytes_read == -1)
+	{
+		close(fd);
+		free(buff);
+		return (0);
+	}
+
+	bytes_written = write(STDOUT_FILENO, buff, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		close(fd);
+		free(buff);
+		return (0);
+	}
+
+	close(fd);
+	free(buff);
+	return (bytes_written);
+}
