f8d67ceed1933b136f0e534bcec5579a8751b5dd
Tasie Ibiam <tasieibiam28@gmail.com>
2024-04-09 22:17:38 +0100

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,10 +13,12 @@ int linear_search(int *array, size_t size, int value)
 	size_t i = 0;
 
 	if (!array)
+	{
 		return (-1);
+	}
 	while (i < size)
 	{
-		printf("Value checked array[%li] = %i\n", i, array[i]);
+		printf("Value checked array[%li] = [%i]\n", i, array[i]);
 		if (array[i] == value)
 		{
 			return (i);
